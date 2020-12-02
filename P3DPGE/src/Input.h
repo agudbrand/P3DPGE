98e74fd46d04a039a5d0404a45298e93fc788f62
dellevelled <delle.caj@gmail.com>
2020-12-02 01:30:03 -0500

    Try to fix input c++ error

--- P3DPGE/src/Input.h
+++ P3DPGE/src/Input.h
@@ -402,8 +402,8 @@ namespace Input {
 
 	//// temp debugging ////
 		inputActions.push_back(InputAction([](olc::PixelGameEngine* p) {
-			AABBCollider aabb = AABBCollider(&Box(), Vector3(1, 1, 1));
-			SphereCollider sphere = SphereCollider(&Sphere(1, -1, Vector3(1, 1, 0)));
+			AABBCollider aabb = AABBCollider(new Box(), Vector3(1, 1, 1));
+			SphereCollider sphere = SphereCollider(new Sphere(1, -1, Vector3(1, 1, 0)));
 			bool test = aabb.CheckCollision(&sphere, true);
 			if (test) {
 				LOG("test: true");
